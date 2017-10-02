ac4ea73a859efa6edc8a6bcd53c16c02c44aad25
James Canning <james@brudil.com>
2017-10-02 19:51:08 +0100

    Implement our own basic notifications

--- content.js
+++ content.js
@@ -1,22 +1,33 @@
 // content.js
+function getNotificationContainer() {
+  const notificationContainer = document.querySelector('.DrinkerNC');
+  return notificationContainer;
+}
 
-const videoContainer = document.querySelector(".video-container")
+function createNotifcationCenter(videoContainer) {
+  const notificationContainer = document.createElement('div');
+  notificationContainer.className = 'DrinkerNC';
+  notificationContainer.style.position = 'fixed';
+  notificationContainer.style.top = '20px';
+  notificationContainer.style.right = '20px';
+  notificationContainer.style.fontSize = '3rem';
+  notificationContainer.style.zIndex = 2000;
+  
+  videoContainer.appendChild(notificationContainer);
+}
 
-const notificationContainer = document.createElement('div');
-notificationContainer.style.position = 'fixed';
-notificationContainer.style.top = '20px';
-notificationContainer.style.right = '20px';
-notificationContainer.style.zIndex = 2000;
 
-videoContainer.appendChild(notificationContainer);
-
-
-function createNotification(message) {
+function addNotification(message, timeout) {
+  console.log('[drinker] adding notification: ' + message);
+  
+  const notificationContainer = getNotificationContainer();
   const notification = document.createElement('div');
   
   notification.innerText = message;
    
-  return notification;
+  notificationContainer.appendChild(notification);
+
+  setTimeout(() => notification.remove(), timeout)
 }
 
 // TIME TO DRINK
@@ -24,25 +35,40 @@ function createNotification(message) {
 
 
 
-setInterval(function(){ 
+function addNeededNotificationsForSecond(){ 
+  var currentTime = (Math.trunc(document.querySelector('.video-container video').currentTime))
+  console.log('[drinker] currentTime: ' + currentTime);
 
 
-var CurTime = (Math.trunc(document.querySelector('.video-container video').currentTime))
-var TestingTest = "Take a drink"
+  if (currentTime == 173) {
+    addNotification('Interlude music - Take a drink!', 5000);
+  }
 
+  if (currentTime == 174) {
+    addNotification('Hahaha, Mark ugh', 5000);
+  }
 
-if (CurTime == 173) {
-        //alert("Interlude music - Take a drink!"); 
-        //$(".PlayerControls--container").notify("Interlude music - Take a drink!", "info");
-        //alertify.message('Interlude music - Take a drink!').bringToFront(); 
-        //Push.create(TestingTest, {
-         //       body: 'Interlude Music',
-         //   });
+  if (currentTime == 176) {
+    addNotification('Dobby changes personality', 5000);
+  }
 
-
+  if (currentTime == 179) {
+    addNotification('Mark is in a club', 5000);
+  }
 }
 
 
-}, 1000);
+function checkForVideo() {
+  const videoContainer = document.querySelector(".video-container")
+  
+  if (!videoContainer) {
+    setTimeout(checkForVideo, 500);    
+    return;
+  }
 
+  console.log('[drinker] found video! starting drinker.')
+  createNotifcationCenter(videoContainer);
+  setInterval(addNeededNotificationsForSecond, 1000);
+}
 
+checkForVideo();
